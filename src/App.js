6756d49e4b28ea4c261dd074538003f4ae896725
asmi-g <108370938+asmi-g@users.noreply.github.com>
2022-12-30 00:16:25 -0500

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from "react"
 import Contact from "./Contact"
+import felix from "./images/felix.png"
+import fluffykins from "./images/fluffykins.png"
+import mailicon from "./images/mailicon.png"
+import mrwhiskerson from "./images/mrwhiskerson.png"
+import phoneicon from "./images/phoneicon.png"
+import pumpkin from "./images/pumpkin.png"
 
 
 function App() {
     return (
         <div className="contacts">
             <Contact
-                img = "mrwhiskerson"
+                img = {mrwhiskerson}
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 email="mr.whiskaz@catnap.meow"
                 />
             <Contact
-                img = "./images/fluffykins.png"
+                img = {fluffykins}
                 name= "Fluffykins"
                 phone="(212) 555-2345"
                 email="fluff@me.com"
                 />
             <Contact
-                img = "./images/felix.png"
+                img = {felix}
                 name="Felix"
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
                 />
             <Contact
-                img = "./images/pumpkin.png"
+                img = {pumpkin}
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
